47c5cb7f707abcbd165fed50a90a4904685ae4e9
Navin Venugopal <navinvenugopal93@gmail.com>
2017-09-30 18:03:10 -0400

    mailer removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,17 +35,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    respond_to do |format|
       if @user.save
-        # Tell the UserMailer to send a welcome Email after save
-        UserMailer.welcome_email(@user).deliver
- 
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      #redirect_to '/login', notice: "Thank you for signing up!"
+      redirect_to '/login', notice: "Thank you for signing up!"
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        render 'new'
     end
   end
 
@@ -83,7 +76,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :role, :first_name, :last_name, :age, :address, :license_number, :credit_card_number)
+    params.require(:user).permit(:email, :password, :password_confirmation, :role, :first_name, :last_name)
   end
-end
 end
