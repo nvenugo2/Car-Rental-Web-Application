d06ed6e36c7028de77cb50109019f3ecc840cb50
Navin Venugopal <navinvenugopal93@gmail.com>
2017-10-05 15:44:52 -0400

    css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 #Rails.application.config.assets.precompile += %w( creative/manifest.js creative/manifest.css )
-Rails.application.config.assets.precompile += %w( main.css )
+#Rails.application.config.assets.precompile += %w( main.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
