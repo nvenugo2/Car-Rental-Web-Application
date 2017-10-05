56ca4099fdc437074e8e9e86597d243080eb18dd
Navin Venugopal <navinvenugopal93@gmail.com>
2017-10-05 00:33:26 -0400

    updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+   config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
